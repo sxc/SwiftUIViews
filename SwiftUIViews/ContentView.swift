ab74b1c922c044851769b7c62b4448b0ab466386
sxc <qiuyequ@icloud.com>
2020-02-24 11:08:53 +0800

    SF Symbols

--- SwiftUIViews/ContentView.swift
+++ SwiftUIViews/ContentView.swift
@@ -14,20 +14,26 @@ struct ContentView: View {
     var body: some View {
         VStack(spacing: 20) {
             
-            Text("Refactoring")
+            Text("Images")
                 .font(.largeTitle)
             
-            Text("Reusing modifiers")
+            Text("Using SF Symbols")
                 .foregroundColor(Color.gray)
             
-            Text("You can put common modifiers on the parent views to be applied to all the child views.")
+            Text("You will see I use icons or symbols to add clarity to what I'm demonstrating. These come from Apple's new symbol font library whicth you can browse using an app called 'SF Symbols'.")
                 .frame(maxWidth: .infinity)
-                .foregroundColor(Color.white)
-                .padding()
+                 .padding()
                 .background(Color.blue)
-            .layoutPriority(1)
+                .foregroundColor(Color.white)
+                .layoutPriority(1)
+            
+            Image(systemName: "hand.thumbsup.fill")
+                .font(.largeTitle)
+            
+            Image("SF Symbols")
         }
         .font(.title)
+        .edgesIgnoringSafeArea(.bottom)
     }
 }
 
